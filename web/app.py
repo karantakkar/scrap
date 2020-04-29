96088a21845156c531433e4aeb21ee5b0979bab6
aniket965 <aniket965.as@gmail.com>
2020-04-29 14:46:13 +0530

    feat:imgs

--- web/app.py
+++ web/app.py
@@ -4,13 +4,17 @@ import pandas as pd
 df = pd.read_excel('test.xlsx')
 app = Flask(__name__)
 
-print(df['NAME'].to_list())
 @app.route('/')
 def hello():
     product_list = df['NAME'].to_list()
     flipkart_price_list = df['price_flipkart'].to_list()
     amazon_price_list = df['price_amazon'].to_list()
-    return render_template('index.html',product_list= enumerate(product_list),flipkart_price_list=flipkart_price_list,amazon_price_list=amazon_price_list)
+    imgs = df['product_img'].to_list()
+    return render_template('index.html',product_list= enumerate(product_list),
+    flipkart_price_list=flipkart_price_list,
+    amazon_price_list=amazon_price_list,
+    imgs=imgs
+    )
 
 if __name__ == '__main__':
     app.run()
